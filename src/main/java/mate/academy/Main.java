c6afb9a8db958a1e84782e8f12c636c718df2e77
Alex <ursusandwolf@protonmail.com>
2026-02-13 13:29:42 +0200

    final fix

--- src/main/java/mate/academy/Main.java
+++ src/main/java/mate/academy/Main.java
@@ -10,7 +10,7 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 public class Main {
-    public static final Logger logger = LogManager.getLogger(AuthenticationServiceImpl.class);
+    public static final Logger logger = LogManager.getLogger(Main.class);
 
     public static void main(String[] args) {
         AuthenticationService authenticationService = new AuthenticationServiceImpl();
--- src/main/java/mate/academy/service/OrderServiceImpl.java
+++ src/main/java/mate/academy/service/OrderServiceImpl.java
@@ -8,7 +8,7 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 public class OrderServiceImpl implements OrderService {
-    public static final Logger logger = LogManager.getLogger(AuthenticationServiceImpl.class);
+    public static final Logger logger = LogManager.getLogger(OrderServiceImpl.class);
 
     @Override
     public Order completeOrder(Long userId) {
